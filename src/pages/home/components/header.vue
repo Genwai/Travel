5ee09381fe462b7db3d212b55d12779dc304e5cb
Genwai <1079096510@qq.com>
2018-05-05 19:47:22 +0800

    add header

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header">
+    <div class="header-container">
+      <div class="header-back">
+          <span class="iconfont icon-back">&#xe624;</span>
+      </div>
+      <div class="header-input">
+          <span class="iconfont icon-input">&#xe632;</span>
+          输入城市/景点/游玩主题</div>
+      <div class="header-city">城市<span class="iconfont icon-city">&#xe600;</span></div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="stylus">
+    // @import '../../../assets/style/vareble.styl'
+    @import '~@/assets/style/vareble.styl'
+// 注释：1rem = htmml :font-size 50px 
+// @ 符号代表src这个目录下
+    .header-container
+        display flex
+        height 0.86rem
+        line-height 0.86rem
+        background $bgColor
+        color  #ffffff
+        font-size 0.28rem        
+        .header-back
+            width .64rem
+            float left
+            .icon-back
+                display block
+                text-align center 
+                font-size 0.4rem
+        .header-input
+            flex 1
+            background #ffffff
+            border-radius 0.1rem
+            margin-top .12rem
+            margin-left .2rem
+            height 0.64rem
+            color #ccc
+            line-height 0.64rem
+            .icon-input
+                padding-left .20rem
+        .header-city
+            width 1.24rem
+            float right 
+            text-align center
+            .icon-city
+                font-size .25rem
+
+
+            
+
+
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+      <home-header></home-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import HomeHeader from './components/header'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+      HomeHeader,
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
